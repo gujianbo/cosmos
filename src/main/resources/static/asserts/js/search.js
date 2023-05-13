c8ed3ac7b49fa8abaa9e385bb783ed6e7c3e44e5
顾剑波 <gujianbobo@gmail.com>
2023-05-13 22:12:37 +0800

    page

--- src/main/resources/static/asserts/js/search.js
+++ src/main/resources/static/asserts/js/search.js
@@ -1,32 +1,44 @@
 
-var search_res = new Vue({
-    el: '.search_res',
+function setCookie(key, value, days) {
+    // 设置过期时间
+    let data = new Date(
+        new Date().getTime() + days * 24 * 60 * 60 * 1000
+    ).toUTCString();
+    document.cookie = key + "=" + value + "; expires=" + data;
+}
+function getCookie(name) {
+    let arr = document.cookie.match(new RegExp("(^| )" + name + "=([^;]*)(;|$)"));
+    if (arr != null) {
+        return arr[2]
+    } else {
+        return null
+    }
+}
+function gen_cookie_uid(){
+    let rand = new Date().getTime()+Math.random()
+    return "cookie_"+btoa(rand)
+}
+function gen_stream_id(uid){
+    let rand = new Date().getTime()+Math.random()
+    return "search_"+uid+"_"+btoa(rand)
+}
+
+var sub = new Vue({
+    el: ".wrap",
     data: {
-        msg: "hello",
-        res_list:[
+        keywords: "",
+        anchor_keywords: "",
+        stream_id: "",
+        uid_code: "",
+        cur_index: 1,
+        next_visible: false,
+        page_size: 10,
+        res_list: [
 
         ],
         loading_visible: false,
         error_visible: false,
         error_msg: "No error!"
-    }
-})
-
-var foot = new Vue({
-    el: '.foot',
-    data: {
-        page_indexs: [],
-        cur_index: 0,
-        next_visible: false
-    }
-})
-
-var sub = new Vue({
-    el: ".search_form",
-    data: {
-        keywords: "",
-        page_index: 1,
-        page_size: 10
     },
     methods:{
         handleSubmit:function(){
@@ -34,16 +46,37 @@ var sub = new Vue({
                 console.log("keywords empty!")
                 return
             }
-            search_res.$data.loading_visible = true
-            search_res.$data.error_visible = false
+            this.res_list = []
+            this.cur_index = 1
+            this.uid_code = getCookie("uid")
+            if(this.uid_code == null){
+                this.uid_code = gen_cookie_uid();
+                setCookie("uid", this.uid_code, 180)
+            }
+            this.anchor_keywords = this.keywords.trim()
+            this.stream_id = gen_stream_id(this.uid_code)
+            this.loading_visible = true
+            this.error_visible = false
+            // this.cur_index++
             axios
-                .get('/search/10002?query='+this.keywords+'&page_size='+this.page_size+'&page_index'+this.page_index)
+                .get('/search/10002?query='+this.anchor_keywords+'&page_size=' +this.page_size+'&page_index='+this.cur_index
+                    +"&stream_id="+this.stream_id+"&cookie_id="+this.uid_code)
                 .then(this.allocate_results)
         },
+
+        getNextPage:function () {
+            // this.cur_index++
+            axios
+                .get('/search/10002?query='+this.anchor_keywords+'&page_size=' +this.page_size+'&page_index='+this.cur_index
+                    +"&stream_id="+this.stream_id+"&cookie_id="+this.uid_code)
+                .then(this.allocate_results)
+        },
+
         allocate_results:function (response) {
             console.log(response)
             if(response.status == 200){
                 if(response.data.rec_num >= 0){
+                    let that = this
                     response.data.rec_result.forEach(function (element, index, array) {
                         let new_item = {
                             id: element.WOSId,
@@ -51,24 +84,25 @@ var sub = new Vue({
                             abstract: element.abstract,
                             authors: element.authors.join(", ")
                         }
-                        search_res.$data.res_list.push(new_item)
+                        that.res_list.push(new_item)
                     })
-                    foot.$data.cur_index += 1
-                    foot.$data.page_indexs.push(foot.$data.cur_index)
+                    this.cur_index += 1
+                    // this.page_indexs.push(this.cur_index)
                     if(response.data.rec_num < this.page_size){
-                        foot.$data.next_visible = false
+                        this.next_visible = false
                     }else{
-                        foot.$data.next_visible = true
+                        this.next_visible = true
                     }
                 }else{
-                    search_res.$data.error_visible = true
-                    search_res.$data.error_msg = "无结果"
+                    this.error_visible = true
+                    this.error_msg = "无结果"
                 }
             }else{
-                search_res.$data.error_visible = true
-                search_res.$data.error_msg = "获取结果失败 " + response.status
+                this.error_visible = true
+                this.error_msg = "获取结果失败 " + response.status
             }
-            search_res.$data.loading_visible = false
-        }
+            this.loading_visible = false
+        },
+
     }
 })
